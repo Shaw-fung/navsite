e27af0fe61aa39893e8e7d72d360e362a2900c93
Shaw-fung <yyhnxgf@gmail.com>
2025-07-15 23:59:29 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -3745,7 +3745,7 @@ async function showFrontend(request) {
     
     <footer class="footer">
       <p>${settings.copyright} Powered by <a href="https://paoto.com">paoto.com</a>.</p>
-	  <p>程序已开源：<a href="https://github.com/Shaw-fung/navsite">Shaw-fung/navsite</p>
+	  <p>程序已开源：<a href="https://github.com/Shaw-fung/navsite">Shaw-fung/navsite</a></p>
       <p>${settings.icpNumber}</p> 
     </footer>
     
